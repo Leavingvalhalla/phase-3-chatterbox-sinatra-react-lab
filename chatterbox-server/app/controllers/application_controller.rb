98d3e29b8ac5ba7dc8a15e47189bc2dafffb87ad
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-17 17:24:54 -0700

    intial commit

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,28 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+  get '/messages' do
+    messages = Message.all.order('created_at asc')
+    messages.to_json
+  end
+
+  post '/messages' do
+    message = {
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at]
+    }
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(body: params[:body], updated_at: [:updated_at])
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+  end
+
 end
